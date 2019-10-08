b2fd23cd5de67bc734ef17449dbd20bd90a42c75
Jonathan Adams <jd.adams16@gmail.com>
2019-10-07 22:37:10 -0500

    Add beginning of example making simulation requests from NodeJS.

---
+++ example.js
@@ -0,0 +1,77 @@
+const _ = require('lodash');
+const bluebird = require('bluebird');
+const Redis = require('ioredis');
+const redis = new Redis();
+const uuid = require('uuid/v4');
+
+const parameters = {
+    baseFuel: {
+        copula: 10,
+        meanlog: 0.25,
+        sdlog: 0.25
+    },
+    solarFlares: {
+        fuel: {
+            meanlog: 0.2,
+            sdlog: 0.5
+        },
+        lambda: 4,
+        shape: 6,
+        scale: 0.75
+    },
+    asteroids: {
+        fuel: {
+            mean: 1.5,
+            sd: 0.25
+        },
+        lambda: 0.000005
+    }
+};
+const jobsQueue = 'jobsQueue';
+
+// Push 100 requests into queue
+const requests = _.chain()
+    .range(100)
+    .map(() => {
+        redis.rpush(jobsQueue, json.stringify(job));
+        return redis.brpop(job.resultsQueue, 0)
+            .then((result) => {
+                result = json.parse(result);
+                if(result.status !== 'succeeded') {
+                    console.error(result.error);
+                    return result.error;
+                } else {
+                    return result.results;
+                }
+            });
+    })
+    .value();
+
+// Make requests
+Promise.map(_.range(100), () => {
+    // Determine unique response queue and use it to build job
+    const resultsQueue = uuid();
+    const job = {
+        package: 'stealTheMoon',
+        func: 'simulateTrip',
+        parameters,
+        resultsQueue,
+        errorQueue: resultsQueue
+    };
+    // Push job to queue
+    redis.rpush(jobsQueue, json.stringify(job));
+    // Perform blocking pop on result queue that will wait for a response
+    return redis.brpop(resultsQueue, 0)
+        .then((resultJSON) => {
+            const result = json.parse(resultJSON);
+            if(result.status !== 'succeeded') {
+                console.error(result.error);
+                return result.error;
+            } else {
+                return result.results;
+            }
+        });
+})
+    .then((results) => {
+        console.log(results);
+    });
